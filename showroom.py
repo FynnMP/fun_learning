7595b0d8d849668a54d57b7d2ed39f42790a08f5
FynnMP <fynn.pohl.fp@gmail.com>
2023-05-08 12:13:42 +0200

    update

--- showroom.py
+++ showroom.py
@@ -77,11 +77,11 @@ while running:
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            exec(open("./menu.py").read())
+            pygame.quit()
+            quit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                running = False
-            elif event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT:
                 # Move to the previous image
                 current_image = (current_image - 1) % len(images)
             elif event.key == pygame.K_RIGHT:
