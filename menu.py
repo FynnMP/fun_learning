b269e020d2fe4910152a30d53f0b04c19f71f8f5
FynnMP <fynn.pohl.fp@gmail.com>
2023-05-23 13:45:20 +0200

    final

--- menu.py
+++ menu.py
@@ -1,4 +1,9 @@
-# Import necessary libraries for creating a GUI
+# This is the main file to run our project, make sure that all necessary libraries are installed and the 
+# folder strucutre is not changed (see Readme for additional information). 
+# Apart from that, just run this file and have fun. 
+
+
+# Import necessary libraries for creating the menu UI
 from tkinter import *
 from tkinter.ttk import *
 import subprocess, sys
@@ -13,8 +18,8 @@ class GameImg:
         self.lbl = Label(window, image=self.im)  # Initiate a label to be positioned on the window
         self.lbl.place(x=dis_x + x_pos, y=y_pos * dis + 0.7 * dis, anchor=CENTER)  # Place the label using the defined coordinates with some fixed addition
 
-# The NewWindowDescr class enables the initiation of "top-level" windows.
-# This means that a new window is opened over the "original" one.
+# The NewWindowDescr class enables the initiation of "top-level" windows
+# This means that a new window is opened over the "original" one
 class NewWindowDescr(Toplevel):
     def __init__(self, master=None, description=None):
         super().__init__(master=window)  # The Tkinter (Tk()) element which is the underlying the initiation window
@@ -27,7 +32,7 @@ class NewWindowDescr(Toplevel):
         descr = Label(self, text=description, font="Helvetica 14", foreground="black", background="darkgrey")  # Description text and all the specifics
         descr.place(relx=0.5, rely=0.5, anchor=CENTER)  # Determine the position of the text element "descr"
 
-# After having defined different classes, the actual window is defined and designed
+# The actual window for the menu is defined and designed
 window = Tk()
 # Create a graphical element of the GUI with the background color white
 canvas = Canvas(bg="white")
@@ -49,8 +54,7 @@ canvas.pack(fill=BOTH, expand=1)
 dis_lt = 110
 dis_lb = 155
 
-# Using the GameImg class to create the thunbnails for the different games with the filenames and the y and x coordinates (for details see GameImg class)
-# Lets play titel picture placement
+# Using the GameImg class to create the thunbnails for the different games/casino games with the filenames and the y and x coordinates (for details see GameImg class)
 learning_im = GameImg("./graphic/menu/learning.png", 1.6)
 casino_im = GameImg("./graphic/menu/roulette_icon.png", 2.6)
 money_im = GameImg("./graphic/menu/money.png", 3.6)
@@ -58,7 +62,6 @@ money_im = GameImg("./graphic/menu/money.png", 3.6)
 
 # setting style of all widgets
 style = Style()
-#style.theme ('aqua', 'clam', 'alt', 'default', 'classic') (used default) --> possibility to select different
 style.theme_use('default')
 # Title of the games and text of the widgets text 
 style.configure("TButton", font="Helvetica 12 bold", background="#9FD0DD", foreground="black") # Buttons
@@ -68,7 +71,7 @@ style.configure("TLabel", font="Helvetica 18 bold", background="white", foregrou
 
 # Help Button
 btn = Button(window, text="Help",style="W.TButton")
-btn.place(relx=0.9, y=30, anchor=CENTER)
+btn.place(relx=0.9, y=30, anchor=CENTER) # Placement of help button
 btn.bind("<Button>", lambda e: NewWindowDescr(window,
                                               "         Welcome to our LEARNxCASINO platform. \n"
                                               "         In the Learning section you can earn money by \n"
@@ -82,92 +85,99 @@ btn.bind("<Button>", lambda e: NewWindowDescr(window,
                                               "         Don´t forget to inspect your purchased items in \n" 
                                               "         high resolution in the showroom."))
 
-# relative placement title and button of game
+# relative placement of titles and buttons for each game / casino game
 rx_title = 0.5
 rx_button1 = 0.2
 rx_button2 = 0.5
 rx_button3 = 0.8
 
-# After the window has been initiated the different sections for the different games are created.
-# In the following five different sections with the same build up are designing the window layout for the games
-# DOTS AND BOXES
-# The Gamename title is created and then placed using the above specified coordinates and relative positions
-l_titel_DB = Label(window, text="Learning")
-l_titel_DB.place(relx=rx_title, y=dis+dis_lt, anchor="center")
-# The description button for Dots and Boxes
+############### Learning
+# The Learning title is created and then placed using the above specified coordinates and relative positions
+l_titel_L = Label(window, text="Learning")
+l_titel_L.place(relx=rx_title, y=dis+dis_lt, anchor="center")
+
+# The function to run the management memory 
 def run_management():
     python_executable = sys.executable
     script_file = "memory_management.py"
     command = [python_executable, script_file]
     subprocess.run(command, check=True)
-btn1 = Button(window, text="Mangement", command = run_management) # Button element with the name Description
+
+# Creating and placing a button to be clicked to start the management memory
+btn1 = Button(window, text="Mangement", command = run_management) # Button element with the name Management
 btn1.place(relx=rx_button1, y=dis+dis_lb, anchor=CENTER) # placing the button
 
+
+# Define function to run accounting memory
 def run_accounting():
     python_executable = sys.executable
     script_file = "memory_accounting.py"
     command = [python_executable, script_file]
     subprocess.run(command, check=True)
-# The play button is created and the command to run the game is bound to it
+
+# Creating and placing a button to be clicked to start the accounting memory
 btn1_3 = Button(window, text="Finance", command = run_accounting)
-# The button is place in the relative section 
 btn1_3.place(relx=rx_button3, y=dis+dis_lb, anchor=CENTER)
 
 
-# CASINO
-l_titel_DB = Label(window, text="Casino")
-l_titel_DB.place(relx=rx_title, y=2*dis+dis_lt, anchor="center")
+############### Casino
+# The Casino title is created and then placed using the above specified coordinates and relative positions
+l_titel_C = Label(window, text="Casino")
+l_titel_C.place(relx=rx_title, y=2*dis+dis_lt, anchor="center")
 
-# Roulette
+# Define function to run roulette
 def run_roulette():
     python_executable = sys.executable
     script_file = "roulette.py"
     command = [python_executable, script_file]
     subprocess.run(command, check=True)
+    
+# Creating and placing a button to be clicked to start the roulette game
 btn2 = Button(window, text="Roulette", command = run_roulette)
 btn2.place(relx=rx_button1, y=2*dis+dis_lb, anchor=CENTER)
 
-# Slots
+# Define function to run the slot machine 
 def run_slots():
     python_executable = sys.executable
     script_file = "slots.py"
     command = [python_executable, script_file]
     subprocess.run(command, check=True)
+
+# Creating and placing a button to be clicked to start the slot machine
 btn2_2 = Button(window, text="Slots", command = run_slots)
 btn2_2.place(relx=rx_button3, y=2*dis+dis_lb, anchor=CENTER)
 
 
 
-# Spending Money
-l_titel_DB = Label(window, text="Spending Money")
-l_titel_DB.place(relx=rx_title, y=3*dis+dis_lt, anchor="center")
+############### Spending Money
+# The Spending Money title is created and then placed using the above specified coordinates and relative positions
+l_titel_M = Label(window, text="Spending Money")
+l_titel_M.place(relx=rx_title, y=3*dis+dis_lt, anchor="center")
 
-# SHOP
+# Define function to open the shop
 def run_shop():
     python_executable = sys.executable
     script_file = "shop.py"
     command = [python_executable, script_file]
     subprocess.run(command, check=True)
+
+# Creating and placing a button to be clicked to open the shop
 btn3 = Button(window, text="Shop", command = run_shop)
 btn3.place(relx=rx_button1, y=3*dis+dis_lb, anchor=CENTER)
 
-# SHOWROOM
+# Define function to open the showroom
 def run_showroom():
     python_executable = sys.executable
     script_file = "showroom.py"
     command = [python_executable, script_file]
     subprocess.run(command, check=True)
+
+# Creating and placing a button to be clicked to open the showroom
 btn3_2 = Button(window, text="Showroom", command = run_showroom)
 btn3_2.place(relx=rx_button3, y=3*dis+dis_lb, anchor=CENTER)
 
-# Current Balance
-# load current balance once
-with open("wallet.json", "r") as wallet:
-            wallet = json.load(wallet)
-            money = wallet["money"]
-
-# define function to consistently update money
-# load current balance once
+############### Current Balance 
+# load current balance once from json file 
 with open("wallet.json", "r") as wallet:
             wallet = json.load(wallet)
             money = wallet["money"]
@@ -178,25 +188,24 @@ def update_balance():
     with open("wallet.json", "r") as wallet:
         wallet = json.load(wallet)
         money = wallet["money"]
+
     # Update the balance label with the new value
-    l_titel_DB.config(text="Your current balance is: %d$" % sum(money))
+    l_titel_B.config(text="Your current balance is: %d$" % sum(money))
     # Call this function again after 1 second
     window.after(1000, update_balance)
 
-# Create the balance label
-l_titel_DB = Label(window, text="Your current balance is: %d$" % sum(money), background="white", foreground="black", font="Helvetica 12 bold")
-l_titel_DB.place(relx=rx_title, y=4*dis+dis_lt+13, anchor="center")
+# Create and place the balance label
+l_titel_B = Label(window, text="Your current balance is: %d$" % sum(money), background="white", foreground="black", font="Helvetica 12 bold")
+l_titel_B.place(relx=rx_title, y=4*dis+dis_lt+13, anchor="center")
 
 # Call the update_balance function to start the loop
 update_balance()
-
-    
 
 # Schedule the function to run the first time
 window.after(1, update_balance)
 
 
-# The before specified window size (wgeox, wgeoy) is now bound to the window. 
+# The before specified window size (wgeox, wgeoy) is now bound to the window
 window.geometry(str(wgeox)+"x"+str(wgeoy)+"+10+10") #10 + 10 specifies the position of the window in on the display of the user when opened
 window.resizable(0, 0) # Ensure the window is not resizable as not all the elements are adjustable
 window.title('Menu') # set the caption for the Inition Window
